b516a10247109f219c7a5fcbaf2bc9fb4f17e5e2
Rinki-Guria218 <rinkiguria218@gmail.com>
2025-08-01 20:34:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+function fun(){
+        document.getElementsByClassName('right')[0].style="background:white;"
+        document.getElementsByClassName('part')[0].style="color:black;"
+        document.getElementsByClassName('part2')[0].style="color:black;"
+                document.getElementsByClassName('part4')[0].style="color:black;"
+                document.getElementsByClassName('contact')[0].style="color:black;"
+
+                document.getElementsByClassName('part5')[0].style="color:black;"
+
+
+                        for(var i=0;i<=10;i++){
+                                 document.getElementsByTagName('button')[i].style="color:black;"
+                                 document.getElementsByClassName('heading')[i].style="color:black;"
+                                document.getElementsByClassName('line2')[i].style="background:black;"
+
+                        }
+
+           
+            
+    }
+    function fun2(){
+        document.getElementsByClassName('right')[0].style="background:#0F1729;"
+         document.getElementsByClassName('part')[0].style="color:white;"
+         document.getElementsByClassName('part2')[0].style="color:white;"
+                  document.getElementsByClassName('part4')[0].style="color:white;"
+
+                  document.getElementsByClassName('part5')[0].style="color:white;"
+                  document.getElementsByClassName('contact')[0].style="color:white;"
+
+
+        for(var i=0;i<=10;i++){
+          document.getElementsByTagName('button')[i].style="color:white;"
+         document.getElementsByClassName('heading')[i].style="color:white;"
+         document.getElementsByClassName('line2')[i].style="color:white;"
+
+                        }
+
+        
+
+    } 
+
+
+        function fun3(){
+                if(document.getElementsByClassName('left')[0].style="display:none"){
+                document.getElementsByClassName('left')[0].style="display:block;"
+                }
+                else{
+                  document.getElementsByClassName('left')[0].style="display:none;"      
+                }
+                }
